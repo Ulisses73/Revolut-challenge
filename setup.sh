8c9207a768f79c20f220467fb72bf1ecb4c16998
Ulisses Rodrigues <ulisses.rodrigues@jumia.com>
2023-09-07 21:14:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@
 export APP_ENV=production
 export FLASK_APP=app.py
 
-# Configure database connection
-export DATABASE_URI=your_database_uri
+# Configure database connection, fill the database_uri with the right one
+export DATABASE_URI=database_uri
 
-# Configure application-specific settings
+# Configure debug and log settings
 export DEBUG=False
 export LOG_LEVEL=info
 
-# Install Python dependencies using a virtual environment
+# Install dependencies using a virtual environment
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
